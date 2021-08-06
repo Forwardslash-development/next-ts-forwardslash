df04fe4656d654b42ef6f1e0ce392ebab48acee7
Forwardslash-development <forwardslash.development@gmail.com>
2021-08-05 20:00:27 -0500

    seo component added

--- config.ts
+++ config.ts
@@ -1,5 +1,9 @@
 const metadata = {
   siteName: 'Forwardslash Development',
+  description: 'Forwardslash Development: Modern Application Development',
+  author: {
+    twitter: '@development_io',
+  },
 }
 
 export { metadata }
